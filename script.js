8dc9810f4f29ff68aa89a578bbc8eea42002a909
Jose Ortega <jose.ort98@gmail.com>
2021-05-01 13:47:59 -0500

    comentatios, Sentencias, Variables, constantes y uso de let

---
+++ script.js
@@ -0,0 +1,79 @@
+
+/*
+alert('Soy JavaScript!')
+console.log(alert);
+
+//Comentarios de linea
+
+
+comentarios de mas de una linea
+*/
+
+//Sentencias
+/*
+alert('Hola'); alert('Mundo');
+
+alert('Hola');
+alert('Mundo');
+
+alert('Hola')
+alert('Mundo')
+*/
+
+// insercion automatica de punto y coma
+
+//variable
+
+let mensaje;
+mensaje = "hola Mundo"
+alert(mensaje)
+
+
+let a = 5;
+let b = 6;
+let suma = a + b;
+console.log("El resultado de la suma es: " + suma )
+
+let nombre = 'Jose';
+let nombre2 = nombre;
+
+nombre2 = 'Armando'
+console.log('El nimbre uno es: ', nombre);
+console.log('El nombre dos es: ', nombre2);
+
+// Nombrar variables con camel case
+nombreUno = 'Jose';
+nombreDos = 'Armando';
+
+// Nombrar con underscore
+nombre_uno = 'Jose';
+nombre_dos = 'Armando';
+
+// Nombres no comunes
+let $ = 1;
+let _ = 2;
+
+// No puede nombrar variables
+
+/*let 1a = 3;
+let primer-nombre = 4; */
+
+"use strict"
+mensaje = "Hola Mundo";
+
+
+//Constantes
+
+const PI = 3.141624;
+const COLOR_ROJO = "#FFFFFF";
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi_dos);
+
+
+const id_account = get_id_account(45);
+
+
+
+
+
